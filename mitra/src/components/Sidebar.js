54b964e74676489b52686953e2fb6890e2bc1d0d
Prerana Rao <mpreranarao@gmail.com>
2023-03-18 01:18:08 +0530

    Adding icons

--- mitra/src/components/Sidebar.js
+++ mitra/src/components/Sidebar.js
@@ -5,39 +5,38 @@ const Sidebar = () => {
         <div>
             <div className="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark" style={{width: "280px", display:"inline", position:"fixed", minHeight:"100%"}}>
     <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-      <svg className="bi me-2" width="40" height="32"></svg>
       <span className="fs-3">MITRA</span>
     </a>
     <hr/>
     <ul className="nav nav-pills flex-column mb-auto">
       <li className="nav-item">
         <a href="#" className="nav-link active" aria-current="page">
-          <svg className="bi me-2" width="16" height="16"></svg>
+        <i class="fa-solid fa-house fa-lg icons"></i>
           <span className="fs-5">Home</span>
         </a>
       </li>
       <li>
         <a href="#" className="nav-link text-white">
-          <svg className="bi me-2" width="16" height="16"></svg>
+        <i class="fa-brands fa-rocketchat fa-lg icons"></i>
           <span className="fs-5">Chat with Mitra</span>
         </a>
       </li>
       <li>
         <a href="#" className="nav-link text-white">
-          <svg className="bi me-2" width="16" height="16"></svg>
+        <i class="fa-solid fa-file-image fa-lg icons"></i>
           <span className="fs-5">Posts</span>
         </a>
       </li>
       <li>
         <a href="#" className="nav-link text-white">
-          <svg className="bi me-2" width="16" height="16"></svg>
+        <i class="fa-solid fa-people-robbery fa-lg icons"></i>
           <span className="fs-5">Socialize</span>
         </a>
       </li>
       <hr/>
       <li>
         <a href="#" className="nav-link text-white">
-          <svg className="bi me-2" width="16" height="16"></svg>
+        <i class="fa-solid fa-gear fa-lg icons"></i>
           <span className="fs-5">Settings</span>
         </a>
       </li>
@@ -45,7 +44,7 @@ const Sidebar = () => {
     <hr/>
     <div className="dropdown">
       <a href="#" className="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-        <img src="https://github.com/mdo.png" alt="" width="32" height="32" className="rounded-circle me-2"/>
+      <i class="fa-solid fa-user fa-lg icons"></i>
         <strong>Username</strong>
       </a>
       <ul className="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
