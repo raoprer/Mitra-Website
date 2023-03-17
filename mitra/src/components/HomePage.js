50eb6555527516f9f3ca8c17a11750d8ad77af64
Prerana Rao <mpreranarao@gmail.com>
2023-03-18 01:35:31 +0530

    Homepage update

--- mitra/src/components/HomePage.js
+++ mitra/src/components/HomePage.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 const HomePage = () => {
     return (
-        <div style={{marginLeft:"280px"}}>
-            <div id="circle-1" class="rounded-circle position-absolute scale-up-center"></div>
+        <div class="container-fluid" style={{marginLeft:"280px"}}>
+            <div class="align-items-center justify-content-between">
+                <h1><strong>Hey,<br></br>Mitra</strong></h1>
+            </div>
+            {/* <div id="circle-1" class="rounded-circle position-absolute scale-up-center"></div>
             <div id="circle-2" class="rounded-circle position-absolute scale-up-center delay-2"></div>
-            <div id="circle-3" class="rounded-circle position-absolute scale-up-center delay-3"></div>        
+            <div id="circle-3" class="rounded-circle position-absolute scale-up-center delay-4"></div>         */}
         </div>
     )
 }
